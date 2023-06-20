f01900b7bc5b20bd7b8133f09a4f76247b6b926b
kumuda345 <kumudachandra1992@gmail.com>
2023-06-20 18:34:12 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to React Test App</h1>
+      <p>This is a sample React.js application for testing purposes.</p>
+    </div>
+  );
+}
+
+export default App;
